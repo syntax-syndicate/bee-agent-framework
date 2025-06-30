3337e8c26dfca98ac9594f249152eac920c53bb8
ioitiki <andymalkin@hotmail.com>
2025-06-30 13:50:30 -0700

    fix: run yarn format:fix for typescript files
    Signed-off-by: ioitiki <andymalkin@hotmail.com>

--- typescript/tests/examples/examples.test.ts
+++ typescript/tests/examples/examples.test.ts
@@ -22,85 +22,80 @@ import { getEnv } from "@/internals/env.js";
 import { ExecException } from "node:child_process";
 
 const execAsync = (command: string) =>
-	new Promise<{ stdout: string; stderr: string }>((resolve, reject) =>
-		exec(
-			command,
-			{
-				shell: process.env.SHELL || "/bin/bash",
-			},
-			(error, stdout, stderr) =>
-				error ? reject(error) : resolve({ stdout, stderr }),
-		),
-	);
+  new Promise<{ stdout: string; stderr: string }>((resolve, reject) =>
+    exec(
+      command,
+      {
+        shell: process.env.SHELL || "/bin/bash",
+      },
+      (error, stdout, stderr) => (error ? reject(error) : resolve({ stdout, stderr })),
+    ),
+  );
 
 const includePattern = process.env.INCLUDE_PATTERN || `./examples/**/*.ts`;
 const excludePattern = process.env.EXCLUDE_PATTERN || ``;
 
 const exclude: string[] = [
-	"examples/workflows/contentCreator.ts",
-	"examples/workflows/competitive-analysis/**/*.ts",
-	"examples/agents/experimental/remote.ts",
-	"examples/playground/**/*.ts",
-	"examples/internals/fetcher.ts",
-	// prevents 'Too many requests' error on Free Tier
-	!getEnv("WATSONX_API_KEY") && [
-		"examples/backend/providers/watson*.ts",
-		"examples/agents/experimental/replan.ts",
-		"examples/agents/experimental/streamlit.ts",
-		"examples/agents/granite/*.ts",
-		"examples/agents/granite/single_turn.ts",
-	],
-	!getEnv("GROQ_API_KEY") && [
-		"examples/agents/sql.ts",
-		"examples/backend/providers/groq.ts",
-		"examples/workflows/*",
-	],
-	!getEnv("OPENAI_API_KEY") && [
-		"examples/agents/bee_reusable.ts",
-		"examples/backend/providers/openai.ts",
-	],
-	!getEnv("AZURE_OPENAI_API_KEY") && [
-		"examples/backend/providers/azure-openai.ts",
-	],
-	!getEnv("COHERE_API_KEY") && ["examples/backend/providers/langchain.ts"],
-	!getEnv("CODE_INTERPRETER_URL") && ["examples/tools/custom/python.ts"],
-	!getEnv("ELASTICSEARCH_NODE") && ["examples/agents/elasticsearch.ts"],
-	!getEnv("AWS_REGION") && ["examples/backend/providers/amazon-bedrock.ts"],
-	!getEnv("GOOGLE_APPLICATION_CREDENTIALS") && [
-		"examples/backend/providers/vertexai.ts",
-	],
-	!getEnv("ANTHROPIC_API_KEY") && ["examples/backend/providers/anthropic.ts"],
-	!getEnv("XAI_API_KEY") && ["examples/backend/providers/xai.ts"],
-	"examples/tools/custom/extending.ts", // DDG problems
+  "examples/workflows/contentCreator.ts",
+  "examples/workflows/competitive-analysis/**/*.ts",
+  "examples/agents/experimental/remote.ts",
+  "examples/playground/**/*.ts",
+  "examples/internals/fetcher.ts",
+  // prevents 'Too many requests' error on Free Tier
+  !getEnv("WATSONX_API_KEY") && [
+    "examples/backend/providers/watson*.ts",
+    "examples/agents/experimental/replan.ts",
+    "examples/agents/experimental/streamlit.ts",
+    "examples/agents/granite/*.ts",
+    "examples/agents/granite/single_turn.ts",
+  ],
+  !getEnv("GROQ_API_KEY") && [
+    "examples/agents/sql.ts",
+    "examples/backend/providers/groq.ts",
+    "examples/workflows/*",
+  ],
+  !getEnv("OPENAI_API_KEY") && [
+    "examples/agents/bee_reusable.ts",
+    "examples/backend/providers/openai.ts",
+  ],
+  !getEnv("AZURE_OPENAI_API_KEY") && ["examples/backend/providers/azure-openai.ts"],
+  !getEnv("COHERE_API_KEY") && ["examples/backend/providers/langchain.ts"],
+  !getEnv("CODE_INTERPRETER_URL") && ["examples/tools/custom/python.ts"],
+  !getEnv("ELASTICSEARCH_NODE") && ["examples/agents/elasticsearch.ts"],
+  !getEnv("AWS_REGION") && ["examples/backend/providers/amazon-bedrock.ts"],
+  !getEnv("GOOGLE_APPLICATION_CREDENTIALS") && ["examples/backend/providers/vertexai.ts"],
+  !getEnv("ANTHROPIC_API_KEY") && ["examples/backend/providers/anthropic.ts"],
+  !getEnv("XAI_API_KEY") && ["examples/backend/providers/xai.ts"],
+  "examples/tools/custom/extending.ts", // DDG problems
 ]
-	.filter(isTruthy)
-	.flat(); // list of examples that are excluded
+  .filter(isTruthy)
+  .flat(); // list of examples that are excluded
 
 describe("E2E Examples", async () => {
-	const exampleFiles = await glob(includePattern, {
-		cwd: process.cwd(),
-		dot: false,
-		realpath: true,
-		ignore: [exclude, excludePattern].flat(),
-	});
+  const exampleFiles = await glob(includePattern, {
+    cwd: process.cwd(),
+    dot: false,
+    realpath: true,
+    ignore: [exclude, excludePattern].flat(),
+  });
 
-	it.concurrent.each(exampleFiles)(`Run %s`, async (example) => {
-		await execAsync(`yarn start -- ${example} <<< "Hello world"`)
-			.then(({ stdout, stderr }) => {
-				// eslint-disable-next-line no-console
-				console.log("STDOUT:", stdout);
-				expect(stderr).toBeFalsy();
-			})
-			.catch((_e) => {
-				const error = _e as ExecException;
+  it.concurrent.each(exampleFiles)(`Run %s`, async (example) => {
+    await execAsync(`yarn start -- ${example} <<< "Hello world"`)
+      .then(({ stdout, stderr }) => {
+        // eslint-disable-next-line no-console
+        console.log("STDOUT:", stdout);
+        expect(stderr).toBeFalsy();
+      })
+      .catch((_e) => {
+        const error = _e as ExecException;
 
-				// eslint-disable-next-line no-console
-				console.error(error.message);
-				// eslint-disable-next-line no-console
-				console.error("STDOUT:", error.stdout);
+        // eslint-disable-next-line no-console
+        console.error(error.message);
+        // eslint-disable-next-line no-console
+        console.error("STDOUT:", error.stdout);
 
-				expect(error.stderr).toBeFalsy();
-				expect(error.code).toBe(0);
-			});
-	});
+        expect(error.stderr).toBeFalsy();
+        expect(error.code).toBe(0);
+      });
+  });
 });
